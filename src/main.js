28aba87a8e4def9b15045a28fc03e64d4194b831
Philipp Munzert <philipp@phmu.de>
2021-02-01 21:40:27 +0100

    add vuex support

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import router from "./router"
+import router from "./router"
+import store from "./store"
 import './index.css'
 
 // use router
 // createApp(App).use(router).mount("#app");
 
-createApp(App).mount("#app");
+// use store and router
+createApp(App).use(router).use(store).mount("#app");
+
+// createApp(App).mount("#app");
