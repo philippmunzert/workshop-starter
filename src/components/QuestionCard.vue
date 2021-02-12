8f90e2ce9175a8c06355d603c1df2445d712f7e0
Philipp Munzert <philipp@phmu.de>
2021-02-12 09:40:41 +0100

    add transitions

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,22 +1,24 @@
 <template>
-  <div
-    v-if="!data.isAnswered && showCard"
-    class="question-card flex justify-center p-4 mx-auto"
-    @click="markAnswered"
-  >
+  <transition appear name="fade">
     <div
-      class="max-w-md border-r w-full border-b border-l border-gray-200 lg:border-l-0 lg:border-t lg:border-gray-200 bg-white rounded p-4 flex flex-col justify-between leading-normal shadow"
+      v-if="!data.isAnswered && showCard"
+      class="question-card flex justify-center p-4 mx-auto"
+      @click="markAnswered"
     >
-      <div class="mb-2">
-        <div class="text-black font-bold text-xl mb-2">{{ data.text }}</div>
-      </div>
-      <div>
-        <div class="text-sm text-center">
-          <p class="text-black leading-none font-bold">{{ data.author }}</p>
+      <div
+        class="max-w-md border-r w-full border-b border-l border-gray-200 lg:border-l-0 lg:border-t lg:border-gray-200 bg-white rounded p-4 flex flex-col justify-between leading-normal shadow"
+      >
+        <div class="mb-2">
+          <div class="text-black font-bold text-xl mb-2">{{ data.text }}</div>
+        </div>
+        <div>
+          <div class="text-sm text-center">
+            <p class="text-black leading-none font-bold">{{ data.author }}</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -37,3 +39,47 @@ export default {
   },
 };
 </script>
+
+<style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  animation: slide-in 0.75s;
+}
+
+.fade-leave-to {
+  animation: slide-out 0.5s;
+  transform: height 1s ease;
+}
+
+@keyframes slide-in {
+  0% {
+    opacity: 0;
+    transform: translateY(100vh);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  0% {
+    transform: translateX(0);
+    max-height: 500px;
+  }
+  75% {
+    opacity: 0;
+    transform: translateX(100vw);
+  }
+
+  100% {
+    transform: translateX(100vw);
+    max-height: 0px;
+    padding: 0;
+  }
+}
+</style>
