259a2b4bcbbecb9646b1944b924c5a01f740bd94
Philipp Munzert <philipp@phmu.de>
2021-02-12 10:04:01 +0100

    move data handling to vuex

--- src/pages/AddNewSessionPage.vue
+++ src/pages/AddNewSessionPage.vue
@@ -3,7 +3,7 @@
     <h1 class="text-3xl leading-normal mb-8 font-bold">
       Starte eine neue Live-Session
     </h1>
-    <form class="w-full max-w-md">
+    <form class="w-full max-w-md" @submit.prevent="createEvent">
       <div class="-mx-3 mb-6">
         <div class="w-full px-3 mb-6">
           <label
@@ -14,11 +14,13 @@
             class="appearance-none block w-full bg-gray-50 text-gray-800 border border-gray-100 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             type="text"
             placeholder="Veranstaltungsname"
+            v-model="sessionName"
           />
         </div>
       </div>
       <button
         class="bg-green-500 w-full text-md hover:bg-green-600 text-white font-semibold py-3 px-12 border border-green-500 rounded shadow"
+        type="submit"
       >
         Neue Session beginnen
       </button>
@@ -27,7 +29,27 @@
 </template>
 
 <script>
+import { format } from "date-fns";
 export default {
   name: "AddNewSessionPage",
+  data() {
+    return {
+      sessionName: "",
+      date: Date.now(),
+    };
+  },
+  methods: {
+    createEvent() {
+      this.$store.dispatch("addSession", {
+        name: this.sessionName,
+        date: `${format(this.date, "dd. MMM")} um ${format(
+          this.date,
+          "HH:MM"
+        )}`,
+      });
+      this.sessionName = "";
+      this.$router.push("/");
+    },
+  },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -4,11 +4,64 @@ import { createStore } from 'vuex'
  const store = createStore({
   state () {
     return {
-      // ...
+        events: [
+            {
+              title: "Vue Workshop Hamburg",
+              timeSlot: "02. Februar um 08:00",
+              slug: "vue-workshop-remote",
+              isLive: true,
+            },
+            {
+              title: "Vue Workshop Hamburg",
+              timeSlot: "25. November um 09:00",
+              slug: "vue-workshop-hamburg-2",
+              isLive: false,
+            },
+            {
+              title: "Vue Workshop Hamburg",
+              timeSlot: "18. November um 09:00",
+              slug: "vue-workshop-hamburg",
+              isLive: false,
+            },
+            {
+              title: "Vue Workshop Berlin",
+              timeSlot: "13. Mai um 09:00",
+              slug: "vue-workshop-berlin",
+              isLive: false,
+            },
+            {
+              title: "Vue Workshop Böblingen",
+              timeSlot: "04. März um 09:00",
+              slug: "vue-workshop-boeblingen",
+              isLive: false,
+            },
+            {
+              title: "Vue Workshop Leipzig",
+              timeSlot: "02. Februar um 09:00",
+              slug: "vue-workshop-leipzig",
+              isLive: false,
+            },
+          ],
     }
   },
   mutations: {
-    // ...
+    ADD_SESSION(state, session) {
+        state.events.unshift(session);
+      }
+  },
+  actions: {
+    addSession({ commit }, session) {
+        // create slug
+        const slug = session.name.toLowerCase().replace(/ /g, "-");
+        // create new session object
+        const newSession = {
+          title: session.name,
+          timeSlot: session.date,
+          slug,
+          isLive: true
+        };
+        commit("ADD_SESSION", newSession);
+      }
   }
 })
 
